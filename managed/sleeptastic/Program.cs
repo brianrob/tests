96d4cb948f93117baa079e3b0a0ee0d593b6829e
Brian Robbins <brianrob@microsoft.com>
2017-11-15 11:37:07 -0800

    Add perpetual sleep test.

---
+++ managed/sleeptastic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace sleeptastic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                SleepSome();
+                Thread.Sleep(10);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        static void SleepSome()
+        {
+            Thread.Sleep(100);
+        }
+    }
+}
