181e07d329617cc3d30f8dc1d1a56fb830b584d4
Brian Robbins <brianrob@microsoft.com>
2017-11-17 12:03:49 -0800

    Add new spinner go app.

--- go/spinner/main.go
+++ go/spinner/main.go
@@ -1,14 +1,34 @@
 package main
 
+import (
+    "log"
+    "net/http"
+    "time"
+)
+
+import "expvar"
+import _ "net/http/pprof"
+
+var spinCount = expvar.NewInt("SpinCount")
+
 func main() {
+
+    go func() {
+        log.Println("Starting HTTP")
+        log.Println(http.ListenAndServe("localhost:6060", nil))
+    }()
+
+    log.Println("Starting spin")
     for {
         spin()
+        spinCount.Add(1)
+        time.Sleep(0)
     }
 }
 
 func spin() {
     sum := 0
-    for i:=0;i<9999999;i++ {
+    for i:=0;i<1000;i++ {
         sum += i
         sum -= i
     }
