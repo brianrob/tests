e48387b0a3024b531953b3172807994bc5c296e0
Brian Robbins <brianrob@microsoft.com>
2018-01-26 13:43:24 -0800

    Implement an allocator test that has a thread with a small stack.

---
+++ managed/allocator_smallstack/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace allocator_smallstack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating allocator thread.");
+            Thread t = new Thread(new ThreadStart(Allocate), 64 * 1024); // 64K stack
+            t.Start();
+            Console.ReadKey();
+
+        }
+
+        static void Allocate()
+        {
+            while(true)
+            {
+                GC.KeepAlive(new object());
+            }
+        }
+    }
+}
