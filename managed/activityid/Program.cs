ebf015d57c0f1ce1c87be3867dec0912ecfca1e7
Brian Robbins <brianrob@microsoft.com>
2018-01-26 14:13:43 -0800

    Add new test to set and use activityId.

---
+++ managed/activityid/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
+
+namespace activityid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guid activityId = Guid.NewGuid();
+            
+            Console.WriteLine($"Setting the activity ID to {activityId.ToString()}.");
+            EventSource.SetCurrentThreadActivityId(activityId);
+            Debug.Assert(activityId == EventSource.CurrentThreadActivityId);
+
+            TestEventSource.Log.TestEvent(1, "hello world!");
+        }
+    }
+
+    [EventSource]
+    public class TestEventSource : EventSource
+    {
+        public static TestEventSource Log = new TestEventSource();
+
+        private TestEventSource() : base(true)
+        {
+        }
+
+        [Event(1)]
+        public void TestEvent(int intArg, string strArg)
+        {
+            WriteEvent(1, intArg, strArg);
+        }
+    }
+}
