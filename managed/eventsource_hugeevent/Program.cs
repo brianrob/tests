3be52b9efd347cd1377ca2c18750c848499bc1e3
Brian Robbins <brianrob@microsoft.com>
2018-03-22 13:30:51 -0700

    Add new hugeevent test.

---
+++ managed/eventsource_hugeevent/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+using System.Text;
+
+namespace eventsource_hugeevent
+{
+    [EventSource]
+    class MySource : EventSource
+    {
+        public static MySource Log = new MySource();
+
+        private MySource()
+            : base()
+        {
+        }
+
+        [Event(1)]
+        public void SendEvent(string s1, string s2)
+        {
+            WriteEvent(1, s1, s2);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Building a very large string.");
+            int stringSize = 90947;
+            StringBuilder builder = new StringBuilder(stringSize);
+            for(int i=0; i<stringSize; i++)
+            {
+                builder.Append('A');
+            }
+            Console.WriteLine("Writing an event with a very large string.");
+            MySource.Log.SendEvent("Foo", builder.ToString());
+        }
+    }
+}
