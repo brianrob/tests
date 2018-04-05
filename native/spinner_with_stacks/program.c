2c3860f4c970e319896b98b4a135e1e62caf94dc
Brian Robbins <brianrob@microsoft.com>
2018-04-05 15:26:22 -0700

    Add a native spinner app that can generate stacks in a trace.

---
+++ native/spinner_with_stacks/program.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void SpinYetAgain()
+{
+    volatile unsigned int count = 0;
+    for(int i=0; i<1000; i++)
+    {
+        count++;
+    }
+}
+
+void SpinSomeMore()
+{
+    volatile unsigned int count = 0;
+    for(int i=0; i<1000; i++)
+    {
+        count++;
+    }
+
+    SpinYetAgain();
+}
+
+void Spin()
+{
+    volatile unsigned int count = 0;
+    for(int i=0; i<1000; i++)
+    {
+        count++;
+    }
+
+    SpinSomeMore();
+}
+
+int main(int argc, char* argv[])
+{
+    while(1)
+    {
+        Spin();
+    }
+}
