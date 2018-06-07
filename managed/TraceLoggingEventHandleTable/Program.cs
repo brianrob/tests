73c6a3310d99d2bd9751782d7f1a8bbe3bdc8edf
Brian Robbins <brianrob@microsoft.com>
2018-06-07 11:41:46 -0700

    Add TraceLoggingEventHandleTable.

---
+++ managed/TraceLoggingEventHandleTable/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TraceLoggingEventHandleMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int numDeclaredEvents = 0; numDeclaredEvents <= 10; numDeclaredEvents++)
+            {
+                for (int numTraceLoggingEvents = 0; numTraceLoggingEvents <= 10000; numTraceLoggingEvents += 2000)
+                {
+                    Console.WriteLine($"Testing: DeclaredEvents = {numDeclaredEvents}, TraceLoggingEvents = {numTraceLoggingEvents}");
+                    Test(numDeclaredEvents, numTraceLoggingEvents);
+                }
+            }
+        }
+
+        private static void Test(int numDeclaredEvents, int numTraceLoggingEvents)
+        {
+            TraceLoggingEventHandleTable handleTable = new TraceLoggingEventHandleTable(numDeclaredEvents);
+
+            for (int i = numDeclaredEvents; i < numDeclaredEvents + numTraceLoggingEvents; i++)
+            {
+                handleTable.SetEventHandle(i, (IntPtr)i);
+                Validate(handleTable, numDeclaredEvents + 1, i + 1, numDeclaredEvents);
+            }
+        }
+
+        private static void Validate(TraceLoggingEventHandleTable handleTable, int startEventID, int endEventID, int numDeclaredEvents)
+        {
+            for (int i = startEventID; i < endEventID; i++)
+            {
+                Debug.Assert((IntPtr)i == handleTable[i]);
+            }
+
+            for (int i = endEventID - numDeclaredEvents; i < handleTable.Length; i++)
+            {
+                Debug.Assert(IntPtr.Zero == handleTable[i+numDeclaredEvents]);
+            }
+        }
+    }
+}
---
+++ managed/TraceLoggingEventHandleTable/TraceLoggingEventHandleTable.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace TraceLoggingEventHandleMap
+{
+    internal sealed class TraceLoggingEventHandleTable
+    {
+        private const int DefaultLength = 10;
+        private int m_numDeclaredEvents;
+        private IntPtr[] m_innerTable;
+        private object m_innerTableUpdateLock = new object();
+        internal TraceLoggingEventHandleTable(int numDeclaredEvents)
+        {
+            if (numDeclaredEvents < 0)
+            {
+                throw new InvalidOperationException();
+            }
+            m_numDeclaredEvents = numDeclaredEvents;
+            m_innerTable = new IntPtr[DefaultLength];
+        }
+
+        internal int Length
+        {
+            get { return Volatile.Read(ref m_innerTable).Length; }
+        }
+
+        internal IntPtr this[int eventID]
+        {
+            get
+            {
+                IntPtr ret = IntPtr.Zero;
+                IntPtr[] innerTable = Volatile.Read(ref m_innerTable);
+
+                int index = EventIDToIndex(eventID);
+                if (index >= 0 && index < innerTable.Length)
+                {
+                    ret = innerTable[index];
+                }
+
+                return ret;
+            }
+        }
+
+        internal void SetEventHandle(int eventID, IntPtr eventHandle)
+        {
+            lock (m_innerTableUpdateLock)
+            {
+                int index = EventIDToIndex(eventID);
+                
+                if(index >= m_innerTable.Length)
+                {
+                    int newSize = m_innerTable.Length * 2;
+                    if (newSize <= index)
+                    {
+                        newSize = index + 1;
+                    }
+
+                    IntPtr[] newTable = new IntPtr[newSize];
+                    Array.Copy(m_innerTable, newTable, m_innerTable.Length);
+                    Volatile.Write(ref m_innerTable, newTable);
+                }
+
+                m_innerTable[index] = eventHandle;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private int EventIDToIndex(int eventId)
+        {
+            // Declared event IDs start at 0 and increase monotonically.
+            // This data structure only contains information on TraceLogging events whose IDs are
+            // assigned after the declared events.
+            return eventId - m_numDeclaredEvents;
+        }
+    }
+}
