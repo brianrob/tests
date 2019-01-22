549575cf2b8088e155bcde2f5c053d7760f4d725
Brian Robbins <brianrob@microsoft.com>
2019-01-22 11:56:58 -0800

    Fix results reporting.

--- managed/mono_time_to_main/src/stop_tracing.sh
+++ managed/mono_time_to_main/src/stop_tracing.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 sudo lttng stop time_to_main
-sudo lttng view | grep mono | grep sched_process_exec
-sudo lttng view | grep mono | grep /function/main
+sudo lttng view | grep mono | grep 'sched_process_exec\|/function/main'
 sudo lttng destroy time_to_main
