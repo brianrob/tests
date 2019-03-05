eca2dba2199d0b2468b70837f37d40a83d903d1a
Brian Robbins <brianrob@microsoft.com>
2019-03-05 15:31:19 -0800

    Add aspnet_start test app.

---
+++ managed/aspnet_start/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace aspnet_start
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime lifetime)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            lifetime.ApplicationStarted.Register(OnAppStarted);
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+
+        private void OnAppStarted()
+        {
+            Program._startupTimer.Stop();
+            Log($"Startup elapsed ms: {Program._startupTimer.ElapsedMilliseconds} ms");
+        }
+
+        private void Log(string s)
+        {
+            string time = DateTime.UtcNow.ToString();
+            Console.WriteLine($"[{time}] {s}");
+        }
+    }
+}
