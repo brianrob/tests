92ec4c9616aca2e142740a02752aea7a8641e4c6
Brian Robbins <brianrob@microsoft.com>
2019-03-12 16:07:18 -0700

    Add Core 3.0 configuration.

--- managed/time_to_main/src/run_core_3.0.sh
+++ managed/time_to_main/src/run_core_3.0.sh
@@ -2,13 +2,13 @@
 
 iterations=10
 echo "----------------------------------------------------------------"
-echo "Running $iterations iterations of Core 2.2 (Plus 1 warm-up iteration.)."
+echo "Running $iterations iterations of Core 3.0 (Plus 1 warm-up iteration.)."
 echo "----------------------------------------------------------------"
 
 for (( i=0; i<=$iterations; i++ ))
 do
     echo "Iteration $i"
-    dotnet bin_core_2.2/Release/netcoreapp2.2/time_to_main.dll
+    dotnet bin_core_3.0/Release/netcoreapp3.0/time_to_main.dll
 done
 
 echo "----------------------------------------------------------------"
