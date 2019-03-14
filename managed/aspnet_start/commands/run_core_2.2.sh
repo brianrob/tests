028add1458f3458876fa3bc2b69c533ca7ad87ca
Brian Robbins <brianrob@microsoft.com>
2019-03-14 14:05:11 -0700

    Add aspnet_start .NET Core commands.

---
+++ managed/aspnet_start/commands/run_core_2.2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+dotnet ~/src/aspnet-benchmarks/src/BenchmarksDriver/bin/Release/netcoreapp2.1/BenchmarksDriver.dll \
+    --server $SERVER_URL \
+    --client $CLIENT_URL \
+    --no-clean \
+    --display-output \
+    --jobs https://raw.githubusercontent.com/brianrob/tests/master/managed/aspnet_start/config/aspnet_start_core_2.2.json \
+    --scenario aspnet_start_core_2.2
