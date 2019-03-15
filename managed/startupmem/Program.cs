944526f39bfcd10dde546f332b247faa16ae8b44
Brian Robbins <brianrob@microsoft.com>
2019-03-15 07:44:21 -0700

    Add startupmem.

---
+++ managed/startupmem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace startupmem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Process.GetCurrentProcess().PrivateMemorySize64);
+            Console.ReadLine();
+        }
+    }
+}
