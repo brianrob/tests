5d66dd6e92258812ae26af684325d8c3daf68f43
Brian Robbins <brianrob@microsoft.com>
2019-04-01 14:08:33 -0700

    Add more TE command files.

---
+++ managed/techempower/commands/run_mono_aot_nollvm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+dotnet ~/src/aspnet-benchmarks/src/BenchmarksDriver/bin/Release/netcoreapp2.1/BenchmarksDriver.dll \
+    --server $SERVER_URL \
+    --client $CLIENT_URL \
+    --no-clean \
+    --jobs https://raw.githubusercontent.com/brianrob/benchmarks/master/src/Benchmarks/benchmarks.te.mono-aot-nollvm.json \
+    --scenario Plaintext-AspNetCoreMono
