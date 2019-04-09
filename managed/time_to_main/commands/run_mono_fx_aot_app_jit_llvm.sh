4538764c446f4ccc0e81871d02df12a6cae438b7
Brian Robbins <brianrob@microsoft.com>
2019-04-09 15:43:11 -0700

    Add new docker/config/command files for time_to_main.

---
+++ managed/time_to_main/commands/run_mono_fx_aot_app_jit_llvm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+dotnet ~/src/aspnet-benchmarks/src/BenchmarksDriver/bin/Release/netcoreapp2.1/BenchmarksDriver.dll \
+    --server $SERVER_URL \
+    --client $CLIENT_URL \
+    --no-clean \
+    --display-output \
+    --jobs https://raw.githubusercontent.com/brianrob/tests/master/managed/time_to_main/config/time_to_main_mono_fx_aot_app_jit_llvm.json \
+    --scenario time_to_main_mono_fx_aot_app_jit_llvm
