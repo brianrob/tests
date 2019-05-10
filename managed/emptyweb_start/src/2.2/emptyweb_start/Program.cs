13de3f2decbff11149173fecdcdc856f44793978
Brian Robbins <brianrob@microsoft.com>
2019-05-10 12:24:23 -0700

    Add 2.2 implementation of emptyweb_start.

---
+++ managed/emptyweb_start/src/2.2/emptyweb_start/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace emptyweb_start
+{
+    public class Program
+    {
+        public static Stopwatch _startupTimer = new Stopwatch();
+
+        public static void Main(string[] args)
+        {
+            _startupTimer.Start();
+            IWebHost webHost = CreateWebHostBuilder(args).Build();
+            Startup.WebHost = webHost;
+            webHost.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
