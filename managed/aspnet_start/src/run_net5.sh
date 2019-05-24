0c8d2189eb83a07f61c7e592c20f17c7c542cb79
Brian Robbins <brianrob@microsoft.com>
2019-05-24 16:05:34 -0700

    Add .NET 5 run script.

---
+++ managed/aspnet_start/src/run_net5.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+iterations=10
+echo "----------------------------------------------------------------"
+echo "Running $iterations iterations of .NET 5 (Plus 1 warm-up iteration.)."
+echo "----------------------------------------------------------------"
+
+for (( i=0; i<=$iterations; i++ ))
+do
+    echo "Iteration $i"
+    $MONOCMD bin_mono/Release/netcoreapp3.0/linux-x64/publish/aspnet_start.exe
+done
+
+echo "----------------------------------------------------------------"
+echo "Finished."
+echo "----------------------------------------------------------------"
