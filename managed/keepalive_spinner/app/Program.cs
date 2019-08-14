e08d4386ff152dba34970a189a2ecc5f9496ceba
Brian Robbins <brianrob@microsoft.com>
2019-08-14 15:12:58 -0700

    Add KeepAlive_Spinner.

---
+++ managed/keepalive_spinner/app/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        private static object Obj = new object();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            while(true)
+            {
+                GC.KeepAlive(Obj);
+            }
+        }
+    }
+}
