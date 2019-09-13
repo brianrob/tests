95477530dd4fa9e84c05b3d3dfe498a546cb365d
Brian Robbins <brianrob@microsoft.com>
2019-09-13 10:19:55 -0700

    Add threadtracking.

---
+++ managed/threadtracking/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace threadtracking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(ThreadProc);
+            t.Start();
+        }
+
+        private static void ThreadProc(object state)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
