36d6eae7c76a19685a496e40640a5bd04231362e
Brian Robbins <brianrob@microsoft.com>
2019-09-13 10:19:12 -0700

    Add eventsource_dictionary.

---
+++ managed/eventsource_dictionary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+using System.Collections.Generic;
+
+namespace eventsource_dictionary
+{
+    public sealed class TestEventSource : EventSource
+    {
+        public static TestEventSource Log = new TestEventSource();
+
+        public TestEventSource() : base(true)
+        {
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,string> foo = new Dictionary<string,string>();
+            foo["foo"] = "bar";
+            TestEventSource.Log.Write<Dictionary<string,string>>("Event",foo);
+//            TestEventSource.Log.Write("Event", new { Payload = foo });
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
