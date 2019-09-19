d287ffa08e21d2d28e5310ee9e3a459562b145d9
Brian Robbins <brianrob@microsoft.com>
2019-09-19 15:29:29 -0700

    Add block counts.

--- managed/FileStomper/Program.cs
+++ managed/FileStomper/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace FileStomper
@@ -38,24 +39,52 @@ namespace FileStomper
 
             Console.WriteLine("Reading the binary back.");
             long zeroBytes = 0;
+
+            SortedDictionary<long, int> spanCountMap = new SortedDictionary<long, int>();
+            long currentSpanLength = 0;
+
             using (StreamReader binaryStreamReader = new StreamReader(BinaryPath))
             {
-                while(!binaryStreamReader.EndOfStream)
+                while(true)
                 {
                     int b = binaryStreamReader.BaseStream.ReadByte();
                     if(b == 0)
                     {
                         zeroBytes++;
+                        currentSpanLength++;
                     }
-                    else if(b == -1)
+                    else
                     {
-                        break;
+                        if(currentSpanLength > 0)
+                        {
+                            int spanCount = 0;
+                            if(spanCountMap.TryGetValue(currentSpanLength, out spanCount))
+                            {
+                                spanCountMap[currentSpanLength] = ++spanCount;
+                            }
+                            else
+                            {
+                                spanCountMap[currentSpanLength] = 1;
+                            }
+                            currentSpanLength = 0;
+                        }
+
+                        if(b == -1)
+                        {
+                            break;
+                        }
                     }
                 }
             }
 
             Console.WriteLine($"Zero Bytes Written: {blocksWritten * BlockSize} ({(blocksWritten * BlockSize) / 1024 }kb)");
             Console.WriteLine($"Zero Bytes Read: {zeroBytes} ({zeroBytes / 1024 }kb)");
+            Console.WriteLine();
+            Console.WriteLine("ChunkSize,Count");
+            foreach(KeyValuePair<long, int> pair in spanCountMap)
+            {
+                Console.WriteLine($"{pair.Key},{pair.Value}");
+            }
         }
     }
 }
