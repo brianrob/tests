37d6295aec5ce2d32354f85feea8b164cf65ada0
Brian Robbins <brianrob@microsoft.com>
2020-07-08 02:46:00 -0700

    Add MinMax threads test.

---
+++ managed/minmax_threads/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace minmax_threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int workerThreads;
+            int ioThreads;
+            ThreadPool.GetMinThreads(out workerThreads, out ioThreads);
+            Console.WriteLine($"GetMinThreads() WorkerThreads({workerThreads}) IOThreads({ioThreads})");
+            ThreadPool.GetMaxThreads(out workerThreads, out ioThreads);
+            Console.WriteLine($"GetMaxThreads() WorkerThreads({workerThreads}) IOThreads({ioThreads})");
+        }
+    }
+}
