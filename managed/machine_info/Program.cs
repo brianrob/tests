dafc8aba422d06de1f51aac54263cf4853d4dbdc
Brian Robbins <brianrob@microsoft.com>
2020-08-27 20:36:35 -0700

    Add initial machine information test app.

---
+++ managed/machine_info/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace machine_info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(DriveInfo driveInfo in DriveInfo.GetDrives())
+            {
+                Console.WriteLine($"Name: {driveInfo.Name}, Format: {driveInfo.DriveFormat}, Type: {driveInfo.DriveType}, Name: {driveInfo.Name}, VolumeLabel: {driveInfo.VolumeLabel}, TotalSize: {driveInfo.TotalSize}, TotalFreeSpace: {driveInfo.TotalFreeSpace}, AvailableFreeSpace {driveInfo.AvailableFreeSpace}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
