7f151ef304f259bc8dd91181fcb87490c442d0be
Brian Robbins <brianrob@microsoft.com>
2020-09-14 13:31:29 -0700

    Update lttng_test app.

--- native/lttng_test/program.c
+++ native/lttng_test/program.c
@@ -1,13 +1,65 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
 #define TRACEPOINT_DEFINE
 #include "tp.h"
 
+void SpinYetAgain()
+{
+    volatile unsigned int count = 0;
+    int i;
+    for(i=0; i<1000; i++)
+    {
+        count++;
+    }
+    usleep(10000);
+}
+
+void SpinSomeMore()
+{
+    volatile unsigned int count = 0;
+    int i;
+    for(i=0; i<1000; i++)
+    {
+        count++;
+    }
+
+    SpinYetAgain();
+}
+
+void Spin()
+{
+    volatile unsigned int count = 0;
+    int i;
+    for(i=0; i<1000; i++)
+    {
+        count++;
+    }
+
+    SpinSomeMore();
+}
+
+void *SpinThreadBase(void *arg)
+{
+    printf("New Thread!\n");
+    tracepoint(test_provider, test_tracepoint, 1, "New Thread!");
+    while(1)
+    {
+        Spin();
+    }
+}
+
 int main(int argc, char* argv[])
 {
-    printf("Writing 10 events.\n");
-    for(int i=0; i<10; i++)
+    pthread_t th;
+
+    if(pthread_create(&th, NULL, SpinThreadBase, NULL))
     {
-        tracepoint(test_provider, test_tracepoint, i+1, "Hello World!");
+        fprintf(stderr, "Error creating thread.\n");
+        return 1;
     }
-    printf("Done.\n");
+
+    SpinThreadBase(NULL);
     return 0;
 }
