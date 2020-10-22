7a702daaba9bb8aad20cb7b40ad9c81a36ee4e3c
Brian Robbins <brianrob@microsoft.com>
2020-10-22 10:42:49 -0700

    Add a Docker deployable system resources console app.

---
+++ managed/system-resources/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace system_resources
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Processor Count: {Environment.ProcessorCount}");
+        }
+    }
+}
