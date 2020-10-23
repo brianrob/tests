fdb6ddba72a088b6c5fe947ae5712512ed8cc1c7
Brian Robbins <brianrob@microsoft.com>
2020-10-23 10:57:20 -0700

    ParallelFor example with reflection hack for maximum concurrency.

---
+++ managed/ParallelFor/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelFor
+{
+    class Program
+    {
+        private static int ConcurrentWorkItems = 0;
+
+        static void Main(string[] args)
+        {
+            InitializeDefaultParallelOptions();
+            Execute();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void InitializeDefaultParallelOptions()
+        {
+            ParallelOptions options = new ParallelOptions()
+            {
+                MaxDegreeOfParallelism = 4
+            };
+
+            // Get the System.Threading.Tasks.Parallel type.
+            Type parallelType = typeof(Parallel);
+            FieldInfo field = parallelType.GetField("s_defaultParallelOptions", BindingFlags.NonPublic | BindingFlags.Static);
+            if(field == null)
+            {
+                throw new Exception("Unable to get field.");
+            }
+
+            field.SetValue(null, options);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Execute()
+        {
+            Console.WriteLine($"Processors: {Environment.ProcessorCount}");
+            int length = 16;
+            int[] list = new int[length];
+
+            Parallel.For(
+                0,
+                list.Length,
+                //new ParallelOptions() { MaxDegreeOfParallelism = 4 },
+                index =>
+                {
+                    int currentCount = Interlocked.Increment(ref ConcurrentWorkItems);
+                    Console.WriteLine($"Index: {index} Thread: {Thread.CurrentThread.ManagedThreadId} CurrentCount: {currentCount}");
+                    Thread.Sleep(1000);
+                    Interlocked.Decrement(ref ConcurrentWorkItems);
+                });
+        }
+    }
+}
