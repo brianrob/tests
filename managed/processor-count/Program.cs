b425cd202eac1d3feed911277bfb54620a008b29
Brian Robbins <brianrob@microsoft.com>
2020-10-26 19:59:45 -0700

    Add a processor count console app.

---
+++ managed/processor-count/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace processor_count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Environment.ProcessorCount={Environment.ProcessorCount}");
+        }
+    }
+}
