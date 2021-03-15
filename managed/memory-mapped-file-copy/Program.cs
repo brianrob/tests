8af8767d625e844c4b6fea53b5623378e8a8df9b
Brian Robbins <brianrob@microsoft.com>
2021-03-15 07:09:36 -0700

    Add memory-mapped apphost copy example.

---
+++ managed/memory-mapped-file-copy/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+
+namespace memory_mapped_file_copy
+{
+    class Program
+    {
+        private const string AppHostPath = @"C:\Program Files\dotnet\sdk\5.0.200-preview.21077.7\AppHostTemplate\apphost.exe";
+
+        static void Main(string[] args)
+        {
+
+            using (MemoryMappedFile srcFile = MemoryMappedFile.CreateFromFile(AppHostPath))
+            {
+                using(MemoryMappedViewStream inStream = srcFile.CreateViewStream())
+                {
+                    using(MemoryMappedViewAccessor accessor = srcFile.CreateViewAccessor())
+                    using (MemoryMappedFile destFile = MemoryMappedFile.CreateNew(null, accessor.Capacity))
+                    {
+                        using (MemoryMappedViewStream outStream = destFile.CreateViewStream())
+                        {
+                            inStream.CopyTo(outStream);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
