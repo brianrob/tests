b40d0905a40832b2dbb3d38f34c2866270b2538e
Brian Robbins <brianrob@microsoft.com>
2021-03-15 07:10:40 -0700

    Add custom MSBuild target example.

---
+++ managed/msbuild-alc/CustomTarget.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Shared;
+using Microsoft.Build.Utilities;
+using System.Runtime.Loader;
+
+namespace msbuild_alc
+{
+    public class CustomTarget : Task
+    {
+        public override bool Execute()
+        {
+            var thisLoadContext = AssemblyLoadContext.GetLoadContext(typeof(CustomTarget).Assembly);
+
+            Log.LogWarning($"AssemblyLoadContext name: \"{thisLoadContext.GetType().Name}\" for current executing assembly {typeof(CustomTarget).Assembly.GetName().Name}");
+
+            return !Log.HasLoggedErrors;
+        }
+    }
+}
