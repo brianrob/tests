72ad983738f62d16257fac249e05188155c91bee
Brian Robbins <brianrob@microsoft.com>
2021-03-15 07:13:30 -0700

    Test app for discovering and modifying windows container file paths from the host.

---
+++ managed/windows_volumes/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace windows_volumes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pid = System.Diagnostics.Process.GetCurrentProcess().Id;
+            Console.WriteLine($"PID: {pid}");
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+
+            DisableLegacyPathHandling();
+
+            string input = @"c:\device\harddiskvolume7\sdk\dotnet-sdk-3.1.401-win-x64\shared\microsoft.netcore.app\3.1.7\coreclr.dll";
+            string output = WindowsDeviceToVolumeMap.Instance.ConvertDevicePathToVolumePath(input);
+
+            Console.WriteLine($"Input: {input}");
+            Console.WriteLine($"Output: {output}");
+            if(File.Exists(output))
+            {
+                File.Copy(output, @"C:\users\brianrob\desktop\coreclr.dll");
+                Console.WriteLine("Copied.");
+            }
+        }
+
+        private static void DisableLegacyPathHandling()
+        {
+            Assembly systemAssembly = Assembly.Load("mscorlib");
+            if (systemAssembly != null)
+            {
+                // Disable the AppContext switch.
+                Type appContextType = systemAssembly.GetType("System.AppContext");
+                if (appContextType != null)
+                {
+                    MethodInfo setSwitchMethod = appContextType.GetMethod("SetSwitch");
+                    if (setSwitchMethod != null)
+                    {
+                        setSwitchMethod.Invoke(null, new object[] { "Switch.System.IO.UseLegacyPathHandling", false });
+                    }
+                }
+
+                // Invalidate the cached copy of the switch.
+                Type appContextSwitchesType = systemAssembly.GetType("System.AppContextSwitches");
+                if(appContextSwitchesType != null)
+                {
+                    FieldInfo useLegacyPathHandlingField = appContextSwitchesType.GetField("_useLegacyPathHandling", BindingFlags.NonPublic | BindingFlags.Static);
+                    if(useLegacyPathHandlingField != null)
+                    {
+                        useLegacyPathHandlingField.SetValue(0, 0);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ managed/windows_volumes/WindowsDeviceToVolumeMap.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace windows_volumes
+{
+    public sealed class WindowsDeviceToVolumeMap
+    {
+        public static WindowsDeviceToVolumeMap Instance = new WindowsDeviceToVolumeMap();
+
+        private const string VolumeNamePrefix = @"\\?\";
+        private const string VolumeNameSuffix = "\\";
+
+        private const string HardDiskVolumePathToken = "Device\\HarddiskVolume";
+        private const int HardDiskVolumePathTokenIndex = 3;
+        private const string VHDHardDiskPathToken = "Device\\VhdHardDisk{";
+        private const int VHDHardDiskPathTokenIndex = 3;
+
+        private Dictionary<string, string> _deviceNameToVolumeNameMap = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+        private WindowsDeviceToVolumeMap()
+        {
+            Initialize();
+        }
+
+        public string ConvertDevicePathToVolumePath(string inputPath)
+        {
+            string updatedPath = inputPath;
+
+            // Check for "Device\HarddiskVolume" pattern.
+            if(inputPath.IndexOf(HardDiskVolumePathToken, 0, StringComparison.OrdinalIgnoreCase) == HardDiskVolumePathTokenIndex)
+            {
+                // Get the device path and see if we have a match.
+                int indexOfSlashAfterDeviceName = inputPath.IndexOf("\\", HardDiskVolumePathTokenIndex + HardDiskVolumePathToken.Length, StringComparison.OrdinalIgnoreCase);
+                string deviceName = inputPath.Substring(2, indexOfSlashAfterDeviceName - 2);
+
+                string volumePath;
+                if(_deviceNameToVolumeNameMap.TryGetValue(deviceName, out volumePath))
+                {
+                    // Get the rest of the path.
+                    string restOfPath = inputPath.Substring(indexOfSlashAfterDeviceName + 1);
+
+                    // Replace the device path with the volume path.
+                    updatedPath = Path.Combine(volumePath, restOfPath);
+                }
+            }
+            // Check for "Device\VhdHardDisk{GUID}" pattern.
+            else if (inputPath.IndexOf(VHDHardDiskPathToken, 0, StringComparison.OrdinalIgnoreCase) == VHDHardDiskPathTokenIndex)
+            {
+                // Get the device path and see if we have a match.
+                int indexOfSlashAfterDeviceName = inputPath.IndexOf("\\", VHDHardDiskPathTokenIndex + VHDHardDiskPathToken.Length, StringComparison.OrdinalIgnoreCase);
+                string deviceName = inputPath.Substring(2, indexOfSlashAfterDeviceName - 2);
+
+                string volumePath;
+                if (_deviceNameToVolumeNameMap.TryGetValue(deviceName, out volumePath))
+                {
+                    // Get the rest of the path.
+                    string restOfPath = inputPath.Substring(indexOfSlashAfterDeviceName + 1);
+
+                    // Replace the device path with the volume path.
+                    updatedPath = Path.Combine(volumePath, restOfPath);
+                }
+            }
+
+            return updatedPath;
+        }
+
+        private void Initialize()
+        {
+            // Create a string builder which will act as the buffer used to receive the volume and device names.
+            StringBuilder builder = new StringBuilder((int)Interop.MAX_PATH, (int)Interop.MAX_PATH);
+
+            // Get the first volume.
+            IntPtr findHandle = Interop.FindFirstVolume(builder, Interop.MAX_PATH);
+            try
+            {
+                do
+                {
+                    string volumeName = builder.ToString();
+                    string deviceName = string.Empty;
+                    string lookupKey = volumeName;
+
+                    // Strip off the volume name prefix and suffix.
+                    if (lookupKey.StartsWith(VolumeNamePrefix))
+                    {
+                        lookupKey = lookupKey.Substring(VolumeNamePrefix.Length);
+                    }
+                    if (lookupKey.EndsWith(VolumeNameSuffix))
+                    {
+                        lookupKey = lookupKey.Substring(0, lookupKey.Length - VolumeNameSuffix.Length);
+                    }
+
+                    // Get the device name.
+                    uint charsWritten = Interop.QueryDosDevice(lookupKey, builder, (int)Interop.MAX_PATH);
+                    if (charsWritten > 0)
+                    {
+                        deviceName = builder.ToString();
+                    }
+
+                    // Save the mapping.
+                    if (volumeName.Length > 0 && deviceName.Length > 0)
+                    {
+                        _deviceNameToVolumeNameMap.Add(deviceName, volumeName);
+                    }
+                }
+                while (Interop.FindNextVolume(findHandle, builder, Interop.MAX_PATH));
+            }
+            finally
+            {
+                // Close the live volume handle.
+                if (findHandle != IntPtr.Zero)
+                {
+                    Interop.FindVolumeClose(findHandle);
+                }
+            }
+        }
+    }
+
+    internal static class Interop
+    {
+        public const UInt32 MAX_PATH = 1024;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern IntPtr FindFirstVolume(
+            [Out] StringBuilder lpszVolumeName,
+            uint cchBufferLength);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern bool FindNextVolume(
+            IntPtr hFindVolume,
+            [Out] StringBuilder lpszVolumeName,
+            uint cchBufferLength);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern bool FindVolumeClose(
+            IntPtr hFindVolume);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern uint QueryDosDevice(
+            string lpDeviceName,
+            StringBuilder lpTargetPath,
+            int ucchMax);
+    }
+}
