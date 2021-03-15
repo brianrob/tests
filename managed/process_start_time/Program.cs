6aea06aeab0d685ade7cff61444bc691002aaff9
Brian Robbins <brianrob@microsoft.com>
2021-03-15 07:12:35 -0700

    Test app for measuring time to main.

---
+++ managed/process_start_time/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace process_start_time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the current process.
+            Process currentProcess = Process.GetCurrentProcess();
+            TimeSpan processTime = DateTime.Now - currentProcess.StartTime;
+            Console.WriteLine($"Execution Time: {processTime}");
+            Console.ReadKey();
+        }
+    }
+}
