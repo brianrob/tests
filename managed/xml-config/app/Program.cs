5da814cb82a12efd98363ef115bfcaa5a458a9a5
Brian Robbins <brianrob@microsoft.com>
2021-03-15 07:08:34 -0700

    Add XML-based config app.

---
+++ managed/xml-config/app/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Diagnostics.Tracing.AutomatedAnalysis;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Configuration configuration = new Configuration();
+            configuration.AddConfigurationFile(@"c:\src\tests\managed\xml-config\test.config.xml");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ managed/xml-config/lib/Configuration.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Microsoft.Diagnostics.Tracing.AutomatedAnalysis
+{
+    public class Configuration
+    {
+        private Dictionary<string, AnalyzerConfiguration> _analyzerConfigurations = new Dictionary<string, AnalyzerConfiguration>();
+
+        internal Configuration()
+        {
+        }
+
+        internal void AddConfigurationFile(string path)
+        {
+            // Open the configuration file.
+            ConfigurationFile file = ConfigurationFile.FromFile(path);
+            foreach(KeyValuePair<string, AnalyzerConfiguration> config in file.Analyzers)
+            {
+                _analyzerConfigurations.Add(config.Key, config.Value);
+            }
+        }
+
+        // TODO: TryGetValue that takes Analyzer as input and returns AnalyzerConfiguration.
+        /* public bool TryGetAnalyzerConfiguration(Analyzer analyzer, out AnalyzerConfiguration config)
+        {
+            return _analyzerConfigurations.TryGetValue(analyzer.GetType().FullName, out config);
+        } */
+    }
+}
