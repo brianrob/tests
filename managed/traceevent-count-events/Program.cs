4a337d3fd157f2e6090d89403743381a8105da67
Brian Robbins <brianrob@microsoft.com>
2021-07-30 17:07:31 -0700

    Add Event Counter that Bails at 20M.

---
+++ managed/traceevent-count-events/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Diagnostics.Tracing;
+
+namespace traceevent_count_events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong eventCount = 0;
+            using (ETWTraceEventSource source = new ETWTraceEventSource(args[0]))
+            {
+                source.AllEvents += delegate (TraceEvent data)
+                {
+                    eventCount++;
+                    if(eventCount > 20000000)
+                    {
+                        source.StopProcessing();
+                    }
+                };
+
+                source.Process();
+            }
+
+            Console.WriteLine($"EventCount = {eventCount}");
+        }
+    }
+}
