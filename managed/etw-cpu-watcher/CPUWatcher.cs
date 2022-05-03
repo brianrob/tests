380f9f9fc5ca0ce8957d0489d8db4204a8cb705c
Brian Robbins <brianrob@microsoft.com>
2022-05-03 11:51:17 -0700

    Sample of live CPU utilization monitor.

---
+++ managed/etw-cpu-watcher/CPUWatcher.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Diagnostics.Tracing.Parsers.Kernel;
+
+namespace etw_cpu_watcher
+{
+    public sealed class CPUWatcher
+    {
+        // It's unlikely that you're modifying the CPU configuration online, so only check once.
+        private static readonly int NumProcs = Environment.ProcessorCount;
+
+        public CPUWatcher(double expectedTimeBetweenSamples)
+        {
+            for (int i = 0; i < CPUs.Length; i++)
+            {
+                CPUs[i] = new CPUState(expectedTimeBetweenSamples);
+            }
+        }
+
+        public void OnCPUSample(SampledProfileTraceData data)
+        {
+            // This operates on a per-CPU basis, but you can also use data.ProcessID to operate on a per-process basis.
+            // If you choose to do this, you should hook Source.Kernel.ProcessStart, Source.Kernel.ProcessDCStart, and Source.Kernel.ProcessStop to keep track of which processes are live.
+            Debug.Assert(data.ProcessorNumber < NumProcs);
+            CPUs[data.ProcessorNumber].OnCPUSample(data);
+        }
+
+        public CPUState[] CPUs { get; } = new CPUState[NumProcs];
+    }
+
+    public sealed class CPUState
+    {
+        private double _totalTime;
+        private double _expectedTimeBetweenSamples;
+        private double _lastTimeStampRelMsec;
+        public CPUState(double expectedTimeBetweenSamples)
+        {
+            _expectedTimeBetweenSamples = expectedTimeBetweenSamples;
+        }
+
+        public void OnCPUSample(SampledProfileTraceData data)
+        {
+            // This can be done in a lock-free manner, but for the purposes of this sample, I'm using a lock for simplicity.
+            lock (this)
+            {
+                // It's possible that the rate of events is faster than we expect, so make sure that we don't add more than the amount of time between samples.
+                _totalTime += Math.Min(_expectedTimeBetweenSamples, data.TimeStampRelativeMSec - _lastTimeStampRelMsec);
+                _lastTimeStampRelMsec = data.TimeStampRelativeMSec;
+            }
+        }
+
+        public double GetTotalTimeAndReset()
+        {
+            // This can be done in a lock-free manner, but for the purposes of this sample, I'm using a lock for simplicity.
+            double ret;
+            lock (this)
+            {
+                ret = _totalTime;
+                _totalTime = 0;
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ managed/etw-cpu-watcher/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.Diagnostics.Tracing.Session;
+
+namespace etw_cpu_watcher
+{
+    class Program
+    {
+        private const float CpuSampleIntervalMsec = 10;
+        private static CPUWatcher Watcher = new CPUWatcher(CpuSampleIntervalMsec);
+        private static Stopwatch Stopwatch = new Stopwatch();
+        private static TraceEventSession Session;
+
+        public static void Main(string[] args)
+        {
+            using (CancellationTokenSource cts = new CancellationTokenSource())
+            {
+                Task processingTask = Task.Factory.StartNew(StartProcessing, cts.Token, cts.Token);
+                Task printingTask = Task.Factory.StartNew(PrintUtilization, cts.Token, cts.Token);
+
+                // Press any key to stop collection.
+                Console.ReadKey();
+                cts.Cancel();
+                Session.Source.StopProcessing();
+                Task.WaitAll(processingTask, printingTask);
+            }
+        }
+
+        private static void StartProcessing(object o)
+        {
+            using (Session = new TraceEventSession("CPUWatcher"))
+            {
+                // Use a stopwatch to keep track of time between printing so that we can calculate percentage utilization.
+                Stopwatch.Start();
+
+                // Enable CPU sampling and set the sampling interval.
+                Session.CpuSampleIntervalMSec = CpuSampleIntervalMsec;
+                Session.EnableKernelProvider(
+                    KernelTraceEventParser.Keywords.Process |
+                    KernelTraceEventParser.Keywords.Profile);
+
+                // Hook the CPU sample event and start processing incoming events.
+                ETWTraceEventSource source = Session.Source;
+                source.Kernel.PerfInfoSample += Watcher.OnCPUSample;
+                Session.Source.Process();
+            }
+        }
+
+        private static void PrintUtilization(object o)
+        {
+            CancellationToken token = (CancellationToken) o;
+            while (!token.IsCancellationRequested)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                double elapsedMilliseconds = Stopwatch.ElapsedMilliseconds;
+
+                // Print the CPU utilization for each processor.
+                for (int i = 0; i < Watcher.CPUs.Length; i++)
+                {
+                    double cpuTime = Watcher.CPUs[i].GetTotalTimeAndReset();
+                    double percentage = cpuTime / elapsedMilliseconds * 100;
+                    percentage = percentage > 100 ? 100 : percentage;
+                    Console.WriteLine($"[{i}] {percentage:N2}");
+                }
+
+                Console.WriteLine();
+                Stopwatch.Restart();
+            }
+        }
+    }
+}
