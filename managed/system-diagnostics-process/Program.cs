7f62495e088cce37367d30468da0f191e40069ce
Brian Robbins <brianrob@microsoft.com>
2022-06-06 11:08:45 -0700

    Spinner that displays CPU and Private Working Set.

---
+++ managed/system-diagnostics-process/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace system_diagnostics_process
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Factory.StartNew(Spin, TaskCreationOptions.LongRunning);
+            while (true)
+            {
+                Process currentProcess = Process.GetCurrentProcess();
+                Console.WriteLine($"Processor Time (ticks): {currentProcess.TotalProcessorTime.Ticks}");
+                Console.WriteLine($"Private Memory: {currentProcess.PrivateMemorySize64:N}");
+                Thread.Sleep(2000);
+            }
+        }
+
+        private static void Spin()
+        {
+            while (true)
+            {
+                Thread.SpinWait(1000);
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
