d7e9e6d2315afa95f8398b95cf15bbe84150e14d
Brian Robbins <brianrob@microsoft.com>
2022-07-08 13:51:57 -0700

    Write-no-execute repro for symbol resolution.

---
+++ managed/write-no-execute-repro/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Test
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            while(true)
+            {
+                Method1();
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Method1()
+        {
+            Method2();
+        }
+        
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Method2()
+        {
+            Method3();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Method3()
+        {
+            Method4();
+        }
+        
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Method4()
+        {
+            GC.KeepAlive(new object());
+        }
+    }
+}
