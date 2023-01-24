67f8537b8608e68e7dc1b2f1340fdf23469b6f5e
Brian Robbins <brianrob@microsoft.com>
2023-01-24 14:28:33 -0800

    EventCounter Listener

---
+++ managed/eventcounter-listener/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Session;
+
+namespace eventcounter_listener
+{
+    class Program
+    {
+        private static readonly string[] Providers = new string[]
+        {
+            "System.Runtime"
+        };
+
+        public static void Main(string[] args)
+        {
+            // Create a new session.
+            using TraceEventSession session = new TraceEventSession("EventCounter-Listener-Session");
+
+            // Register for CTRL+C.
+            Console.CancelKeyPress += delegate (object sender, ConsoleCancelEventArgs cancelArgs)
+            {
+                session.Dispose();
+                Environment.Exit(0);
+            };
+
+
+            TraceEventProviderOptions options = new TraceEventProviderOptions();
+            options.AddArgument("EventCounterIntervalSec", "1");
+
+            foreach (string provider in Providers)
+            {
+                Guid providerGuid = TraceEventProviders.GetEventSourceGuidFromName(provider);
+
+                session.EnableProvider(
+                    providerGuid,
+                    providerLevel: (TraceEventLevel) 1,
+                    matchAnyKeywords: 0,
+                    options: options);
+            }
+
+
+            ETWTraceEventSource eventSource = session.Source;
+
+            // Register for events from each provider.
+            foreach (string provider in Providers)
+            {
+                eventSource.Dynamic.AddCallbackForProviderEvent(
+                    provider,
+                    "EventCounters",
+                    (data) =>
+                    {
+                        Console.WriteLine(data.ToString());
+                    });
+            }
+
+            // Start processing events.
+            eventSource.Process();
+        }
+    }
+}
